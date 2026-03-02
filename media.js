d3a1cb652105e83f001056e24aff9287bf215797
narutoemail46-hub <narutoemail46@gmail.com>
2026-03-02 22:46:40 +0100

    Update media.js

--- media.js
+++ media.js
@@ -1,25 +1,10 @@
 async function media(url) {
-    const response = await request.get(url);
-    const html = response.data;
-    const streams = [];
-
-    // Sucht nach den Hoster-Buttons (Vidoza, Streamtape, etc.)
-    const hosterLinks = html.select('a.btn-hoster');
-
-    hosterLinks.forEach((link) => {
-        const hosterUrl = link.attr('href');
-        const hosterName = link.text().trim();
-
-        if (hosterUrl) {
-            streams.push({
-                name: hosterName,
-                url: hosterUrl,
-                isExternal: true // Da Sora die Hoster meist extern öffnen muss
-            });
-        }
-    });
-
-    return streams;
+    const res = await request.get(url);
+    const links = res.data.select('a.btn-hoster');
+    return links.map(l => ({
+        name: l.text().trim(),
+        url: l.attr('href'),
+        isExternal: true
+    }));
 }
-
 module.exports = { media };
