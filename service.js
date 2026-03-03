21709e637d37d97b5851fdc18be41311ec4a1cd9
narutoemail46-hub <narutoemail46@gmail.com>
2026-03-03 16:49:07 +0100

    Update service.js

--- service.js
+++ service.js
@@ -1,21 +1,18 @@
-// service.js - Minimale Version
+// service.js - GANZ EINFACHE VERSION
 async function extractDetails(url) {
-    return [
-        { title: "One Piece", thumbnail: "", link: "https://anime-base.net/anime/one-piece" },
-        { title: "Jujutsu Kaisen", thumbnail: "", link: "https://anime-base.net/anime/jujutsu-kaisen" }
-    ];
+    return [];
 }
 
 async function searchResults(keyword) {
-    return extractDetails();
+    return [];
 }
 
 async function extractEpisodes(url) {
-    return [{ title: "Episode 1", url: "https://anime-base.net/episode/1", number: 1 }];
+    return [];
 }
 
 async function extractStreamUrl(url) {
-    return "https://test-stream.com/video.mp4";
+    return null;
 }
 
 module.exports = {
